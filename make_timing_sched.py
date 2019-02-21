b9e70b6bf2e959c2c1dd9ae514e001b3ec2ae119
Kelley M. Hess <hess@astro.rug.nl>
2019-02-21 15:21:03 +0100

    Uncommented out atdbquery call.

--- make_timing_sched.py
+++ make_timing_sched.py
@@ -156,16 +156,16 @@ print("\n##################################################################")
 print("Number of all-sky fields are: {}".format(len(fields)))
 print("Number of Timing fields are: {}".format(len(timing_fields)))
 
-# # Retrieve names of observations from ATDB (excludes calibrator scans)
-# observations = atdbquery.atdbquery(obs_mode='SC4')
-# timing_obs = [dict(observations[i])['name'] for i in range(len(observations))
-#               if (dict(observations[i])['name'][0:2] != '3c') and (dict(observations[i])['name'][0:2] != '3C')
-#               and (dict(observations[i])['name'][0:2] != 'CT')]
-# # Adjust 'weights' field for objects that have been previously observed:
-# for obs in timing_obs:
-#     if obs in fields['name']:
-#         i = np.where(timing_fields['name'] == obs)
-#         timing_fields['weights'][i] -= 1
+# Retrieve names of observations from ATDB (excludes calibrator scans)
+observations = atdbquery.atdbquery(obs_mode='SC4')
+timing_obs = [dict(observations[i])['name'] for i in range(len(observations))
+              if (dict(observations[i])['name'][0:2] != '3c') and (dict(observations[i])['name'][0:2] != '3C')
+              and (dict(observations[i])['name'][0:2] != 'CT')]
+# Adjust 'weights' field for objects that have been previously observed:
+for obs in timing_obs:
+    if obs in fields['name']:
+        i = np.where(timing_fields['name'] == obs)
+        timing_fields['weights'][i] -= 1
 
 # Estimate the telescope starting position as on the meridian (approximately parked)
 telescope_position = SkyCoord(ra=Time(obstimeUTC).sidereal_time('apparent', westerbork().lon), dec='50d00m00s')
