e28b57f3c388146b830ce5ba498fee34b17dd965
Leon Oostrum <leonoostrum@gmail.com>
2019-03-27 13:58:48 +0100

    Observe a pulsar calibrator every 12 hours

--- modules/calibrators.py
+++ modules/calibrators.py
@@ -6,6 +6,8 @@ flux_names = ['3C147', '3C196']
 flux_cal = [SkyCoord.from_name(name) for name in flux_names]
 pol_names = ['3C138', '3C286', 'CTD93', '3C48']
 pol_cal = [SkyCoord.from_name(name) for name in pol_names]
+psr_names = ['B1933+16', 'B0531+21', 'B0329+54', 'B0950+08']
+psr_cal = [SkyCoord.from_name('PSR '+name) for name in psr_names]
 
 if __name__ == '__main__':
     print("Flux calibrators are:")
@@ -14,3 +16,6 @@ if __name__ == '__main__':
     print("Polarization calibrators are:")
     for i in range(len(pol_names)):
         print('\t({}) {}: {}'.format(i, pol_names[i], pol_cal[i].to_string('hmsdms')))
+    print("Pulsar calibrators are:")
+    for i in range(len(psr_names)):
+        print('\t({}) {}: {}'.format(i, psr_names[i], psr_cal[i].to_string('hmsdms')))
